d4e4587a883c1a8e79138d90ffb4356ea698ede5
yinxiong007 <50572238+yinxiong007@users.noreply.github.com>
2019-11-01 18:26:42 +0800

    “增加分组删除界面元素”

--- api-auto-test/PO/group_delete.py
+++ api-auto-test/PO/group_delete.py
@@ -16,6 +16,12 @@ class group_delPage(base_page.Action):
     shubiao_loc = (By.XPATH,'//*[@id="app"]/div/div[3]/section/section/main/div[1]/div[2]/table/thead/tr/th[2]/div')
     #删除分组
     group_del_loc = (By.XPATH,'//*[@id="app"]/div/div[3]/section/section/main/div[1]/div[4]/div[2]/table/tbody/tr[1]/td[4]/div/button[2]')
+
+    #删除界面api分组按钮
+    group_del_api = (By.CSS_SELECTOR,'body > div.el-message-box__wrapper > div > div.el-message-box__btns > button.el-button.el-button--default.el-button--small.el-button--primary > span')
+    #删除界面关闭按钮
+    group_del_close = (By.CSS_SELECTOR,'body > div.el-message-box__wrapper > div > div.el-message-box__header > button > i')
+
     #确定
     queding_loc = (By.CSS_SELECTOR,'body > div.el-message-box__wrapper > div > div.el-message-box__btns > button.el-button.el-button--default.el-button--small.el-button--primary')
     #取消
@@ -33,6 +39,14 @@ class group_delPage(base_page.Action):
         #点击分组删除按钮
         self.find_element(*self.group_del_loc).click()
         time.sleep(2)
+
+        #获取删除界面api管理标签内容
+    def get_group_del_api(self):
+        return self.find_element(*self.group_del_api).text
+
+        #关闭删除界面
+    def click_group_del_close(self):
+        self.find_element(*self.group_del_close).click()
 
     def click_queding_loc(self):
         self.find_element(*self.queding_loc).click()
